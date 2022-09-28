e784220629e43dc7f0dc15a060234488b02f6376
Dezinater <15971253+Dezinater@users.noreply.github.com>
2022-09-27 20:07:18 -0400

    use certain amount of workers instead of worker per chunk

--- src/LZ4Writer.ts
+++ src/LZ4Writer.ts
@@ -1,6 +1,7 @@
 let lz4 = require("./lz4/lz4.js");
 //import { Worker } from 'worker_threads';
 
+const WORKERS_AMOUNT = 8;
 //new Worker(new URL('./worker.js'));
 //let lz4 = require("lz4js");
 import { BLOCK_SIZE } from "./LZ4Helper";
@@ -14,19 +15,29 @@ export default class LZ4Writer {
     _buffer: Uint8Array;
     _bufferLength: number;
     currentOutput: Uint8Array;
+    workersStarted: number;
+    workersDone: number;
     readPosition: number;
     writePosition: number;
     canWrite: boolean;
     finalOutput: Uint8Array;
     writeChunks: Array<Uint8Array>;
+    workers: Array<Worker>;
 
     constructor(bytes: ArrayBuffer) {
-        this.writeChunks = new Array(0);
+        this.bytes = bytes;
+        this.canWrite = true;
         this.readPosition = 0;
         this.writePosition = 0;
-        this.canWrite = true;
-        this.bytes = bytes;
+        this.writeChunks = new Array(0);
         this.currentOutput = new Uint8Array(new ArrayBuffer(this.bytes.byteLength));
+
+        this.workersStarted = 0;
+        this.workersDone = 0;
+        this.workers = new Array(WORKERS_AMOUNT);
+        for (let i = 0; i < WORKERS_AMOUNT; i++) {
+            this.workers.push(new Worker(new URL('./worker.js', import.meta.url)));
+        }
     }
 
     workersFinished() {
@@ -54,93 +65,38 @@ export default class LZ4Writer {
                 return;
             }
 
-            let workersDone = 0;
-            let workersRequired = Math.ceil(this.bytes.byteLength / BLOCK_SIZE);
-            for (let i = 0; i < workersRequired; i++) {
-                let worker = new Worker(new URL('./worker.js', import.meta.url));
 
-                let size = Math.min(this.bytes.byteLength - (i * BLOCK_SIZE), BLOCK_SIZE);
-                worker.postMessage({
-                    index: i,
-                    bytes: this.bytes.slice(i * BLOCK_SIZE, (i * BLOCK_SIZE) + size) //since only the last one will be not block_size, i * BLOCK_SIZE is fine
-                });
+            let workersRequired = Math.ceil(this.bytes.byteLength / BLOCK_SIZE);
+
+            this.workers.forEach(worker => {
+                this.startWorker(worker);
 
                 worker.onmessage = (e) => {
-                    workersDone++;
+                    this.workersDone++;
                     let { index, bytes } = e.data;
                     this.writeChunks[index] = bytes;
 
-                    if (workersDone == workersRequired) {
-                        this.workersFinished();
-                        this.canWrite = false;
-                        resolve(this.finalOutput);
+                    if (this.workersDone >= workersRequired) {
+                        if (this.canWrite) {
+                            this.workersFinished();
+                            this.canWrite = false;
+                            resolve(this.finalOutput);
+                        }
+                    } else {
+                        this.startWorker(worker);
                     }
                 };
-            }
+            });
         });
-        /*
-                while (this.canRead) {
-                    this.FlushCurrentChunk();
-                    console.log("Flushed " + this.readPosition + " " + this.writePosition + " " + this.bytes.byteLength);
-                }
-        
-                let finalMap = new Uint8Array(this.writePosition + 4);
-                finalMap[0] = 9;
-                finalMap.set(this.currentOutput.slice(0, this.writePosition), 4);
-        
-                this.currentOutput = finalMap;
-                return this.currentOutput;
-                */
     }
 
-    FlushCurrentChunk() {
-        let compressed = new Array(BLOCK_SIZE);
-        let size = Math.min(this.bytes.byteLength - this.readPosition, BLOCK_SIZE);
-        //console.log(size);
-        let compressedLength = lz4.compressBlock(this.bytes, compressed, this.readPosition, size, new Uint32Array(1 << 14));
-        compressed = compressed.slice(0, compressedLength);
-        this.readPosition += size;
-        //console.log(compressed);
-        //console.log(compressedLength);
-
-        if (compressedLength <= 0 || compressedLength >= BLOCK_SIZE) {
-            // incompressible block
-            //compressed = this.bytes;
-            compressedLength = this.bytes.byteLength;
-        }
-
-        var isCompressed = compressedLength < BLOCK_SIZE;
-        var flags = ChunkFlags.None;
-
-        if (isCompressed) flags |= ChunkFlags.Compressed;
-
-        this.WriteVarInt(flags);
-        //this.WriteVarInt(BLOCK_SIZE);
-        this.WriteVarInt(size);
-        if (isCompressed) this.WriteVarInt(compressedLength);
-
-        this.InnerStreamWrite(new Uint8Array(compressed), 0, compressedLength);
-
-        if (this.readPosition >= this.bytes.byteLength) {
-            this.canWrite = false;
-        }
+    startWorker(worker: Worker) {
+        let size = Math.min(this.bytes.byteLength - (this.workersStarted * BLOCK_SIZE), BLOCK_SIZE);
+        worker.postMessage({
+            index: this.workersStarted,
+            bytes: this.bytes.slice(this.workersStarted * BLOCK_SIZE, (this.workersStarted * BLOCK_SIZE) + size) //since only the last one will be not block_size, i * BLOCK_SIZE is fine
+        });
+        this.workersStarted++;
     }
 
-    WriteVarInt(value: number) {
-        let bufferVal;
-        while (true) {
-            var b = (value & 0x7F);
-            value >>= 7;
-            bufferVal = (b | (value == 0 ? 0 : 0x80));
-            this.currentOutput[this.writePosition] = bufferVal;
-            this.writePosition++;
-            // _innerStream.Write(buffer, 0, 1);
-            if (value == 0) break;
-        }
-    }
-
-    InnerStreamWrite(input: Uint8Array, offset: number, length: number) {
-        memcpy(offset, this.writePosition, length, input, this.currentOutput);
-        this.writePosition += length;
-    }
 }
