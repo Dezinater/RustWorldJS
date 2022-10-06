8fe1d582f95ea9cedeb65a11a659a8b3ebf6e7b5
Dezinater <15971253+Dezinater@users.noreply.github.com>
2022-10-05 20:23:37 -0400

    textmap getDst convert to uint8array

--- src/rust/TextMap.ts
+++ src/rust/TextMap.ts
@@ -3,6 +3,11 @@ export default class TextMap {
 
     constructor(data: Uint8Array) {
         //map each byte value to a character code then combine the array into a string
-        this.dst = Array.from(data).map(x => String.fromCharCode(x)).join(""); 
+        this.dst = Array.from(data).map(x => String.fromCharCode(x)).join("");
     }
+
+    getDst() {
+        return new Uint8Array(this.dst.split("").map(x => x.charCodeAt(0)));
+    }
+
 }
