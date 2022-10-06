3f3552eba428d18ef84b6266faa471b092637680
Dezinater <15971253+Dezinater@users.noreply.github.com>
2022-10-06 17:05:30 -0400

    ice walls write file

--- examples/ice-walls/index.js
+++ examples/ice-walls/index.js
@@ -1,22 +1,31 @@
 const fs = require("fs");
 const rustWorld = require("rustworldjs");
 
-fs.readFile(__dirname + '/../../test/test.map', function (err, fileContents) {
+const inputMapFile = "/../../test/test"; //don't include map extension here
+
+fs.readFile(__dirname + inputMapFile + '.map', function (err, fileContents) {
     let world = rustWorld.readMap(fileContents);
 
     let terrainMap = world.getTerrainMap("terrain");
     let splatMap = world.getTerrainMap("splat");
     let center = splatMap.res / 2; //x and y are same since maps are always square
 
-    fillCircleNegative(terrainMap, center, 420, 255, 1);
-    fillCircleNegative(splatMap, center, 420, 255, 1);
+    fillCircleNegative(terrainMap, center, 420, 1);
+    fillCircleNegative(splatMap, center, 420, 1, 1);
+
+    rustWorld.writeMap(world).then(bytes => {
+        fs.writeFile(__dirname + inputMapFile + '-ice-wall.map', bytes, "binary", function (err) {
+            if (err) return console.log(err);
+            console.log("Finished creating ice walls!")
+        });
+    });
 });
 
 function fillCircleNegative(map, center, radius, value, channel = 0) {
     for (let x = 0; x < map.res; x++) {
         for (let y = 0; y < map.res; y++) {
             if (Math.sqrt((x - center) ** 2 + (y - center) ** 2) > radius) {
-                map.set(value, x, y, channel);
+                map.setNormalized(value, x, y, channel);
             }
         }
     }
