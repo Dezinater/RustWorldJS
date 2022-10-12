6b3ecbbd9974571a578b664c430783eb425de33f
Dezinater <15971253+Dezinater@users.noreply.github.com>
2022-10-12 16:07:44 -0400

    add resize for terrain and new way of storing terrain data

--- src/rust/TerrainMap.ts
+++ src/rust/TerrainMap.ts
@@ -1,14 +1,15 @@
 export default class TerrainMap {
     type: string;
     res: number;
-    dst: Uint8Array | Uint16Array | Uint32Array;
     channels: number;
     worldSize: number;
+    data: Array<Uint8Array | Uint16Array | Uint32Array>;
 
     constructor(data: Uint8Array, channels: number, type: string | "int" | "short" | "byte", worldSize: number) {
         this.channels = channels;
         this.worldSize = worldSize;
         let offset = data.byteOffset;
+        let dst: Uint8Array | Uint16Array | Uint32Array;
 
         //not sure why the data isnt aligned but aligning it like this fixes it
         if (type == "int" && offset % 4 != 0) {
@@ -19,35 +20,60 @@ export default class TerrainMap {
         }
 
         switch (type) {
-            case "int": this.dst = new Uint32Array(data.buffer, offset, data.byteLength / Uint32Array.BYTES_PER_ELEMENT); break;
-            case "short": this.dst = new Uint16Array(data.buffer, offset, data.byteLength / Uint16Array.BYTES_PER_ELEMENT); break;
-            case "byte": this.dst = new Uint8Array(data.buffer, offset, data.byteLength / Uint8Array.BYTES_PER_ELEMENT); break;
+            case "int": dst = new Uint32Array(data.buffer, offset, data.byteLength / Uint32Array.BYTES_PER_ELEMENT); break;
+            case "short": dst = new Uint16Array(data.buffer, offset, data.byteLength / Uint16Array.BYTES_PER_ELEMENT); break;
+            case "byte": dst = new Uint8Array(data.buffer, offset, data.byteLength / Uint8Array.BYTES_PER_ELEMENT); break;
         }
-        
+
+        this.data = new Array();
+        let sliceSize = dst.length / channels;
+        this.res = Math.sqrt(dst.length / channels);
+
+        for (let i = 0; i <= channels - 1; i++) {
+            let slice = dst.slice(i * sliceSize, (i + 1) * sliceSize);
+            let resizedSlice = this.resize(slice, this.res, this.res, worldSize, worldSize);
+            this.data.push(resizedSlice);
+        }
+
         this.type = type;
-        let byteSize = this.BytesPerElement();
-        this.res = Math.ceil(Math.sqrt(data.length / byteSize / channels));
+    }
+
+    resize(pixels, w1, h1, w2, h2) { //nearest neighbour
+        let temp = new pixels.constructor(Array(w2 * h2));
+        let x_ratio = w1 / w2;
+        let y_ratio = h1 / h2;
+        let px, py;
+        for (let i = 0; i < h2; i++) {
+            for (let j = 0; j < w2; j++) {
+                px = Math.floor(j * x_ratio);
+                py = Math.floor(i * y_ratio);
+                temp[(i * w2) + j] = pixels[(py * w1) + px];
+            }
+        }
+        return temp;
     }
 
     getDst() {
-        return new Uint8Array(this.dst.buffer, this.dst.byteOffset, this.dst.byteLength);
+        let resizedData = this.data.map(x => this.resize(x, this.worldSize, this.worldSize, this.res, this.res));
+        let size = resizedData[0].byteLength; //all of them should be the same length
+
+        let finalArray = new resizedData[0].constructor(new ArrayBuffer(size * resizedData.length));
+        resizedData.forEach((data, i) => {
+            finalArray.set(data, i * size);
+        });
+
+        let returnArray = new Uint8Array(finalArray.buffer, finalArray.byteOffset, finalArray.byteLength);
+        //console.log(returnArray);
+        return returnArray;
     }
 
-    convertCoords(x, y) {
-        return {
-            x: Math.ceil((x / this.worldSize) * this.res),
-            y: Math.ceil((y / this.worldSize) * this.res),
-        };
+    get(x = 0, y = 0, channel = 0) {
+        console.log(x, y, (x * this.worldSize) + y);
+        return this.data[channel][(x * this.worldSize) + y];
     }
 
-    get(mapX = 0, mapY = 0, channel = 0) {
-        let { x, y } = this.convertCoords(mapX, mapY);
-        return this.dst[(x + (this.res - y - 1) * this.res) + (channel * this.res * this.res)];
-    }
-
-    set(value: number, mapX = 0, mapY = 0, channel = 0) {
-        let { x, y } = this.convertCoords(mapX, mapY);
-        this.dst[(x + (this.res - y - 1) * this.res) + (channel * this.res * this.res)] = value;
+    set(value: number, x = 0, y = 0, channel = 0) {
+        this.data[channel][(x * this.worldSize) + y] = value;
     }
 
     getNormalized(x = 0, y = 0, channel = 0) {
@@ -61,7 +87,7 @@ export default class TerrainMap {
     }
 
     getChannel(channel: number) {
-        return this.dst.slice(channel * this.res * this.res, (channel + 1) * this.res * this.res)
+        return this.data[channel];
     }
 
     channelsAmount() {
